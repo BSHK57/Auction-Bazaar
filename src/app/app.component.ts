89cba4874b51ab68ba88bae416316e92db40e9a6
B SAI HARI KRISHNA <166925264+BSHK57@users.noreply.github.com>
2024-12-24 11:59:31 +0000

    landingpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Auction_bazaar';
+  isDropdownVisible: boolean = false;
+  constructor(private router: Router) {}
+
+  toggleDropdown(state: boolean): void {
+    this.isDropdownVisible = state;
+  }
+
+  logout(): void {
+    // Implement logout functionality
+    console.log('User logged out');
+    this.router.navigate(['/login']);
+    
+  }
 }
